5458ce1d8c8ae32f723349e038bdb0be40157125
strongestavenger <mikemutoro@gmail.com>
2019-06-11 12:13:59 +0300

    add file operations and link

--- Files/files.py
+++ Files/files.py
@@ -3,4 +3,4 @@ file = open('sample.txt', 'r')
 print(file.read(10))
 
 # more materials
-# https://www.tutorialspoint.com/python/python_files_io.htm
+# https://github.com/andela/learningmap/tree/58baf4a7d9a97a1a3c3863f759b2a5c2c924bc86/D2/D2%20Python/Curriculum/10.%20Input%20-%20Output%20and%20Files 
