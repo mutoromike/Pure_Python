b1b29f80007f82eab93485366eb6047ef87d29f8
mutoromike <mikemutoro@gmail.com>
2023-10-23 12:28:06 +0300

    Update inner function

--- functions/inner_functions.py
+++ functions/inner_functions.py
@@ -1,8 +1,13 @@
 def power(exponent):
     def inner(base):
         return base**exponent
+        """
+        Inner in this case is a closure, it remembers the value of exponent,
+        even after the outer function has finished executing.
+        """
 
     return inner
+
 
 square = power(2)
 cube = power(3)
