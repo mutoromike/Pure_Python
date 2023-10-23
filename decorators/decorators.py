9a6fd55fe4436c572ec4c2cb7eed1259e8c6e932
mutoromike <mikemutoro@gmail.com>
2023-10-23 12:38:21 +0300

    Decorators init

---
+++ decorators/decorators.py
@@ -0,0 +1,4 @@
+"""
+    A decorator is a higher order function that takes a function as an
+    argument and returns a closure (the enclosed function).
+"""
--- functions/inner_functions.py
+++ functions/inner_functions.py
@@ -2,8 +2,13 @@ def power(exponent):
     def inner(base):
         return base**exponent
         """
-        Inner in this case is a closure, it remembers the value of exponent,
+        Inner in this case is a CLOSURE (encloses the needed non-local variable
+        together with the function object), it remembers the value of exponent,
         even after the outer function has finished executing.
+
+        Exponent is a non-local variable for inner. It is also called
+        a cell variable (a reference to the exponent variable from the
+        outer scope).
         """
 
     return inner
